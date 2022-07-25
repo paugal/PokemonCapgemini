dc531432e38a76ec7c20c71777931b05e57f5509
mriverre8 <rivemark00@gmail.com>
2022-07-25 12:07:27 +0200

    Bag items and image png

--- src/components/Bag/Bag.tsx
+++ src/components/Bag/Bag.tsx
@@ -5,8 +5,13 @@ function Bag(props: any) {
   return props.trigger ? (
     <div className="popup">
       <div className="popup-inner">
-        <button className="close-btn" onClick={() => props.setTrigger(false)} >close</button>
-        <h3>holamundoiso</h3>
+        <div className="flex-container">
+          <button className="close-btn" onClick={() => props.setTrigger(false)} >close</button>
+          <button className="close-btn" >Pokedex</button>
+          <button className="close-btn" >Items</button>
+          <button className="close-btn" >Mapa</button>
+          <button className="close-btn" >Reloj</button>
+        </div>
       </div>
     </div>
   ) : (
