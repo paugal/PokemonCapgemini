f0638d99b68a4b842bce9fb87d2c1ed12105a09b
mriverre8 <rivemark00@gmail.com>
2022-07-27 11:59:50 +0200

    Responsive bag

--- src/components/Bag/Bag.tsx
+++ src/components/Bag/Bag.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-import "./Bag.css"
+import "./Bag.css";
+import ButtonBag from "./ButtonBag";
 
 function Bag(props: any) {
   return props.trigger ? (
     <div className="popup">
       <div className="popup-inner">
+        
         <div className="flex-container">
-          <button className="close-btn" onClick={() => props.setTrigger(false)} >close</button>
-          <button className="close-btn" >Pokedex</button>
-          <button className="close-btn" >Items</button>
-          <button className="close-btn" >Mapa</button>
-          <button className="close-btn" >Reloj</button>
+          <button className="close-btn" onClick={() => props.setTrigger(false)}>
+            close
+          </button>
+          <ButtonBag></ButtonBag>
+          <ButtonBag></ButtonBag>
+          <ButtonBag></ButtonBag>
         </div>
       </div>
     </div>
---
+++ src/components/Bag/ButtonBag.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ButtonBag.css"
+
+function ButtonBag() {
+  return (
+    <button className="buttonBag"> Hola </button>
+  )
+}
+
+export default ButtonBag;
